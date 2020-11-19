20c3aa456cc3334cf4acbe6499a5dd771fed8684
Sam Ozminkowski <samoz@umich.edu>
2020-11-19 12:01:03 -0500

    improve progress notifications, fix if statement for checking if files downloaded correctly

--- downloadSNP.sh
+++ downloadSNP.sh
@@ -1,4 +1,5 @@
 #!\bin\bash
+ 
 # this script downloads all quality_variant files from the 1001 genome project. The files selected are those
 # listed at http://signal.salk.edu/atg1001/download.php. Files are downloaded from 
 # http://signal.salk.edu/atg1001/data/Salk/quality_variant_[FILENAME].txt and stored in the 
@@ -28,21 +29,27 @@ cd data
 
 numfls=`echo $flnms | wc -l | sed -E 's/ //g'`
 count=1
+tothash=20
 
 # loop over the filenames
 echo $flnms | while read flnm
-do
-    printf "downloading file "$count" of "$numfls$'\r'
+do  
+    # print our progress to stdout
+    numhash=$(( $count * $tothash / $numfls ))
+    space=`printf ' %.0s' {1..$(( $tothash - $numhash ))}`
+    hash=`printf '#%.0s' {0..$numhash}`
+    printf "downloading file "$count" of "$numfls"  |"$hash$space"|"'\r'
     count=$(( $count + 1 ))
+
+    # print the file name to the logfile so we know what file each line is referring to
     echo $flnm >> "log.txt"
     # download the actual files from the salk website
     # TODO: rewrite to use one curl request for all the files?
     curl "http://signal.salk.edu/atg1001/data/Salk/quality_variant_"$flnm".txt" -O 2>> "log.txt"
 
     # make sure this file was downloaded correctly
-    # TODO: check for 404 error
     if [ ! -f "quality_variant_"$flnm".txt" ] || [ `wc -l "quality_variant_"$flnm".txt" | sed -E 's/ //g'` = 0 ] \
-        || [ ! `head -n 1 "quality_variant_"$flnm".txt" | grep -E  "xml.*encoding.*>"` = "" ]
+        || [ `head -n 1 "quality_variant_"$flnm".txt" | grep -q -E "xml.*encoding.*>"` ]
     then
         echo "\n"$flnm" not downloaded. See log.txt for details."
     fi 

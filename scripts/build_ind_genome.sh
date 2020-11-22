802d7f0d71a7714fa2ef11a83bebd2edd19cdb7e
Nathan Kiel <nkiel@wisc.edu>
2020-11-22 11:02:13 -0600

    added count of sequence length and finding reference genome file

--- scripts/build_ind_genome.sh
+++ scripts/build_ind_genome.sh
@@ -7,14 +7,22 @@
 # snp is a directory with the SNP files from the Salk Arabidopsis website
 # Not added to the git for size considerations
 file_function() {
-    for file in snp/*.txt
+    for file in snp/*.txt # will later have to be changed to data/, and this will change some of below script
     do
-        ls -1U snp | wc -l >> "$1_00$2_00$3".phy
-        # Counts number of files in snp directory 
-        # which are the number of sequences considered
+        num_seq=$(ls -1U snp | wc -l)
+        # Counts number of files in snp directory which are the number of sequences considered
+        length=$(expr $3 - $2)
+        # Counts number of pairs as length of sequence
+        echo "$num_seq $length" >> "$1_00$2_00$3".phy
         # Creates new file where 1 = chromosome of choice (e.g., chr1)
         # 2 = starting position, and 3 = ending position
+        ref=$(find ref_genome/ -name "*$1.fas")
+        ref_name=$(basename -s ".fas" "$ref")
+        echo "From reference genome $ref_name"
+        # to make sure base pairs are pulled from correct reference genome
+        
     done
 }
 # Example use: file_function chr1 102 132 creates chr1_00102_00132.phy
+# and echos "From reference genome TAIR10_chr1"
 # Issue is only ever adds two padding 0's no matter numbers used

85aa377a62ceadc985f371d52740bd9282512103
Evan Gorstein <egorstein@wisc.edu>
2020-11-23 11:22:58 -0600

    Changed script names

--- scripts/build_ind_genome.sh
+++ scripts/build_ind_genome.sh
@@ -1,30 +1,65 @@
-# This script is for Task 3
-# COMMENTS TO BE DELETED BUT READ NOW
-# Since this requires specified genome range of interest,
-# I thought a function would work well here
-# then building a for loop inside the function 
+#Chromosome, starting, and ending positions are given as command line arguments.
+#Chromosome given as number 1 through 5, C or M.
+#Example call: "bash scripts/build_ind_genome.sh C 1568 1892"
+#Let's start by assigning these command line arguments to new variable names
 
-# snp is a directory with the SNP files from the Salk Arabidopsis website
-# Not added to the git for size considerations
-file_function() {
-    for file in snp/*.txt # will later have to be changed to data/, and this will change some of below script
-    do
-        num_seq=$(ls -1U snp | wc -l)
-        # Counts number of files in snp directory which are the number of sequences considered
-        length=$(expr $3 - $2 + 1)
-        # Counts number of pairs as length of sequence
-        echo "$num_seq $length" >> "$1_00$2_00$3".phy
-        # Creates new file where 1 = chromosome of choice (e.g., chr1)
-        # 2 = starting position, and 3 = ending position
-        ref=$(find ref_genome/ -name "*$1.fas")
-        ref_name=$(basename -s ".fas" "$ref")
-        echo "From reference genome $ref_name"
-        # to make sure base pairs are pulled from correct reference genome
+
+chrom="chr$1" 
+startpos=$2
+endpos=$3
+numdiscrep=0 #Number of discrepancies between reference genome and reference nucleotides in SNP file in range of interest
+
+
+numstrands=$(ls -1q data/quality_variant*.txt | wc -l) #Number of strands that we have
+((seqlength = $3-$2+1)) #Length of sequence
+printf -v startnum "%06d" $2 #padding with zeros
+printf -v endnum "%06d" $3 #padding with zeros
+echo "${numstrands} ${seqlength}" > alignments/"$chrom"_"$startnum"_"$endnum".phy #Create output file with first line
+
+
+#Sequence of nucleotides in the reference genome
+reference=$(tail -n +2 data/TAIR10_chr"$1".fas | tr -d '\n' | tail -c +$2 | head -c $seqlength)
+
+
+#We now build each strand's sequence based on the reference sequence and the list of its SNP's.
+for file in data/quality_variant*.txt
+do
+    indseq=$reference 
+
+    #Create a temporary file snp.txt with list of SNP's in range of interest
+    #Each line of snp.txt is an SNP in range of interest 
+    awk -v awk_chrom="$chrom" -v awk_startpos=$startpos -v awk_endpos=$endpos '$2 ~ awk_chrom && $3 <= awk_endpos &&  $3 >= awk_startpos { print $3 "\t" $4 "\t" $5}' $file > snp.txt
+    
+    #We iterate through the lines of snp.txt to make the necessary edits to indseq 
+    while read line; do
+        position=$(echo $line | awk '{print $1}')
+        refbase=$(echo $line | awk '{print $2}')
+        subbase=$(echo $line | awk '{print $3}')
+        dif=`expr $position - $startpos`
         
-        tail +2 $ref | tr -d '\n' | head -c $3 | tail -c $length >> "$1_00$2_00$3".phy
-        # returns correct bases from reference genome, yet to be replaced with SNP bases
-    done
-}
-# Example use: file_function chr1 102 132 creates chr1_00102_00132.phy
-# and echos "From reference genome TAIR10_chr1"
-# Issue is only ever adds two padding 0's no matter numbers used
+        #Check that reference genome and reference nucleotide in SNP file are the same. 
+        #If they aren't, print error.
+        #If they are, insert subbase into indseq.
+        if [ $refbase != ${indseq:$dif:1} ]
+        then 
+            echo "Discrepancy between reference genome and reference nucleotides in SNP files at position $position"
+            ((numdiscrep++))
+        else 
+            ((dif++))
+            indseq=$(echo $indseq | sed "s/./$subbase/$dif")
+        fi
+    done < snp.txt
+
+    #Add line to output file
+    strand=$(head -1 $file | awk '{print $1}')
+    echo "$strand  $indseq" >> alignments/"$chrom"_"$startnum"_"$endnum".phy
+    echo "Allignment of strand $strand" generated
+done
+
+rm snp.txt #Delete temp file snp.txt
+
+#Any discrepancies?
+if [ $numdiscrep -eq 0 ] 
+then 
+    echo "No discrepancies between reference genome and reference nucleotides in any of the SNP files!"
+fi

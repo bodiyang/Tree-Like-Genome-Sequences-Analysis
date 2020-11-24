e04841efc3c937bfe01db023368c100c69148f55
Nathan Kiel <nkiel@wisc.edu>
2020-11-24 14:36:30 -0600

    changed variable names for consistency across scripts

--- scripts/build_ind_genome.sh
+++ scripts/build_ind_genome.sh
@@ -4,9 +4,9 @@
 #Let's start by assigning these command line arguments to new variable names
 
 
-chrom="chr$1" 
-startpos=$2
-endpos=$3
+chromosome="chr$1" 
+start_pos=$2
+end_pos=$3
 numdiscrep=0 #Number of discrepancies between reference genome and reference nucleotides in SNP file in range of interest
 
 
@@ -14,7 +14,7 @@ numstrands=$(ls -1q data/quality_variant*.txt | wc -l) #Number of strands that w
 ((seqlength = $3-$2+1)) #Length of sequence
 printf -v startnum "%06d" $2 #padding with zeros
 printf -v endnum "%06d" $3 #padding with zeros
-echo "${numstrands} ${seqlength}" > alignments/"$chrom"_"$startnum"_"$endnum".phy #Create output file with first line
+echo "${numstrands} ${seqlength}" > alignments/"$chromosome"_"$startnum"_"$endnum".phy #Create output file with first line
 
 
 #Sequence of nucleotides in the reference genome
@@ -28,14 +28,14 @@ do
 
     #Create a temporary file snp.txt with list of SNP's in range of interest
     #Each line of snp.txt is an SNP in range of interest 
-    awk -v awk_chrom="$chrom" -v awk_startpos=$startpos -v awk_endpos=$endpos '$2 ~ awk_chrom && $3 <= awk_endpos &&  $3 >= awk_startpos { print $3 "\t" $4 "\t" $5}' $file > snp.txt
+    awk -v awk_chrom="$chromosome" -v awk_startpos=$start_pos -v awk_endpos=$end_pos '$2 ~ awk_chrom && $3 <= awk_endpos &&  $3 >= awk_startpos { print $3 "\t" $4 "\t" $5}' $file > snp.txt
     
     #We iterate through the lines of snp.txt to make the necessary edits to indseq 
     while read line; do
         position=$(echo $line | awk '{print $1}')
         refbase=$(echo $line | awk '{print $2}')
         subbase=$(echo $line | awk '{print $3}')
-        dif=`expr $position - $startpos`
+        dif=`expr $position - $start_pos`
         
         #Check that reference genome and reference nucleotide in SNP file are the same. 
         #If they aren't, print error.
@@ -52,7 +52,7 @@ do
 
     #Add line to output file
     strand=$(head -1 $file | awk '{print $1}')
-    echo "$strand  $indseq" >> alignments/"$chrom"_"$startnum"_"$endnum".phy
+    echo "$strand  $indseq" >> alignments/"$chromosome"_"$startnum"_"$endnum".phy
     echo "Allignment of strand $strand" generated
 done
 

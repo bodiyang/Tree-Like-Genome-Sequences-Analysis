b257829bc67b0a13192d79332d61824073c0d2f8
Sam Ozminkowski <samoz@umich.edu>
2020-12-04 10:12:44 -0500

    output block order into reference file to confirm correct order

--- scripts/calc_tree_distances.sh
+++ scripts/calc_tree_distances.sh
@@ -5,12 +5,14 @@
 # bash calc_tree_distances.sh 1
 # output for this script will be in the treedist directory in files named
 # chrX-all.tre (all trees in one file, in the order they exist in the directory)
+# chrX-all-ref.txt (list of blocks in the same order as trees in chrX-all.tre, to confirm order is correct)
 # chrX-all_pairs.rfdist and chrX-adj_pairs.rfdist (distances between trees)
 
 # first input should be chromosome number
 chrom_full="chr"$1
 all_file_dir="treedist/"$chrom_full"-all.tre"
 all_file=$chrom_full"-all.tre"
+all_ref="treedist/"$chrom_full"-all-ref.txt"
 
 # if the file exists start by deleting it
 if [[ -f $all_file_dir ]]
@@ -18,10 +20,16 @@ then
     rm $all_file_dir
 fi
 touch $all_file_dir
+if [[ -f $all_ref ]]
+then
+    rm $all_ref
+fi
+touch $all_ref
 
 # gather each tree into 1 file, 1 tree per line
 for tree in iqtree/$chrom_full*.treefile
 do
+    echo $tree >> $all_ref
     cat $tree >> $all_file_dir 
 done
 
